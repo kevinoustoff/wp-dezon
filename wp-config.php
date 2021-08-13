1f0fc988e1fdf18e16579e64a80af7a0704f41b2
KevinToffa <garrickervin@gmail.com>
2021-08-13 05:33:13 +0000

    2021

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'dezon' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'root2021' );
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost');
 
